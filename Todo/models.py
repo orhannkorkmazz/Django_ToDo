fd4ec6d8b6f7c7a1478559610e11113d2cfdab9c
Orhan Korkmaz <orhan.korkmaz@hotmail.com.tr>
2023-09-06 17:05:51 +0300

    first commit

---
+++ Todo/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+class Todo(models.Model):
+    author=models.ForeignKey("auth.user",on_delete=models.CASCADE)
+    title=models.CharField(max_length=50,verbose_name="Başlık")
+    content=models.TextField()
+    created_date=models.DateTimeField(auto_now_add=True,verbose_name="Oluşturulma Tarihi")
+def __str__(self):
+    return self.title
+# Create your models here.
